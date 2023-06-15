eda88b0757e75187b0df4a1dc6c41036b3542fd9
ahauserus <ahauserus@gmail.com>
2023-06-15 06:45:41 +0300

    bubble sort algorithm

--- sort-algorithms/main-sort.c
+++ sort-algorithms/main-sort.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 	int array[10] = {25, 10, 19, 16, -6, -14, 12, 10, 3, 1};
-	int n = sizeof(array) / sizeof(array[0]);
+	int n = sizeof(array) / sizeof(array[0]), i;
 
 	bubbleSort(array, n);
 
 	printf("Sorted array:\n");
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		printf("%d ", array[i]);
 	printf("\n");
 
